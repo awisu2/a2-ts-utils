75ef9f574036b1fbb85330ff88336167e421ee2e
awisu2 <awisu2@gmail.com>
2026-05-06 08:13:31 +0900

    update log

--- packages/common/src/log/log.ts
+++ packages/common/src/log/log.ts
@@ -7,6 +7,15 @@ export const LogLevel = {
 } as const;
 export type LogLevel = (typeof LogLevel)[keyof typeof LogLevel];
 
+export const LogLevelForce = {
+  traceF: "traceF",
+  debugF: "debugF",
+  infoF: "infoF",
+  warnF: "warnF",
+  errorF: "errorF",
+} as const;
+export type LogLevelForce = (typeof LogLevelForce)[keyof typeof LogLevelForce];
+
 const logHierarchy = {
   [LogLevel.trace]: 0,
   [LogLevel.debug]: 1,
@@ -65,9 +74,13 @@ export const setThresholdLevel = (level: LogLevel) => {
   filterLoggers();
 };
 
-const _log: Record<LogLevel, Log> = {} as any;
-const _glog: Record<LogLevel, GLog> = {} as any;
-const _clog: Record<LogLevel, GLog> = {} as any;
+const getLevelForceFromLevel = (level: LogLevel): LogLevelForce => {
+  return (level + "F") as LogLevelForce;
+};
+
+const _log: Record<LogLevel | LogLevelForce, Log> = {} as any;
+const _glog: Record<LogLevel | LogLevelForce, GLog> = {} as any;
+const _clog: Record<LogLevel | LogLevelForce, GLog> = {} as any;
 
 const _logF: Record<LogLevel, Log> = {} as any;
 const _glogF: Record<LogLevel, GLog> = {} as any;
@@ -89,6 +102,11 @@ const filterLoggers = () => {
     _log[level] = active ? _logF[level] : emptyLogger;
     _glog[level] = active ? _glogF[level] : emptyLogger;
     _clog[level] = active ? _clogF[level] : emptyLogger;
+
+    const levelForce = getLevelForceFromLevel(level);
+    _log[levelForce] = _logF[level];
+    _glog[levelForce] = _glogF[level];
+    _clog[levelForce] = _clogF[level];
   }
 };
 
@@ -96,20 +114,31 @@ const filterLoggers = () => {
 setForceLoggers();
 filterLoggers();
 
-export const log: Record<LogLevel, Log> = {} as any;
-export const glog: Record<LogLevel, GLog> = {} as any;
-export const clog: Record<LogLevel, GLog> = {} as any;
+export const log: Record<LogLevel | LogLevelForce, Log> = {} as any;
+export const glog: Record<LogLevel | LogLevelForce, GLog> = {} as any;
+export const clog: Record<LogLevel | LogLevelForce, GLog> = {} as any;
 
 for (const level of Object.values(LogLevel)) {
   log[level] = (...args) => _log[level](...args);
   glog[level] = (name, ...args) => _glog[level](name, ...args);
   clog[level] = (name, ...args) => _clog[level](name, ...args);
+
+  const levelForce = getLevelForceFromLevel(level);
+  log[levelForce] = (...args) => _log[levelForce](...args);
+  glog[levelForce] = (name, ...args) => _glog[levelForce](name, ...args);
+  clog[levelForce] = (name, ...args) => _clog[levelForce](name, ...args);
 }
 
 // just try functions =====
 // generate prefixed log function
-export const prefixedLog = (logger: Log, prefix: unknown): Log => {
+export const prefixedLog = (logger: Log, ...prefixes: unknown[]): Log => {
   return (...args: unknown[]) => {
-    logger(prefix, ...args);
+    logger(...prefixes, ...args);
+  };
+};
+
+export const prefixedGLog = (logger: GLog, ...prefixes: unknown[]): GLog => {
+  return (groupName: string, ...args: unknown[]) => {
+    logger(groupName, ...prefixes, ...args);
   };
 };
