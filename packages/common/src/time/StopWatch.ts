66fab098bf366781ee685fbd64fb001c60045c1b
awisu2 <awisu2@gmail.com>
2026-05-20 13:54:17 +0900

    update StopWatch

--- packages/common/src/time/StopWatch.ts
+++ packages/common/src/time/StopWatch.ts
@@ -1,16 +1,26 @@
+export type StopWatchElapsed = {
+  id: string;
+  total: number;
+  delta: number;
+};
 export class StopWatch {
   private _startTime: number;
   private _latestElapsed: number = 0;
+  private _onElapsed?: (elapsed: StopWatchElapsed) => void;
 
-  constructor() {
+  constructor(onElapsed?: (elapsed: StopWatchElapsed) => void) {
     this._startTime = performance.now();
+    this._onElapsed = onElapsed;
   }
 
-  elapsed(): { total: number; delta: number } {
+  elapsed(id: string = ""): StopWatchElapsed {
     const total = performance.now() - this._startTime;
     const delta = total - this._latestElapsed;
 
     this._latestElapsed = total;
-    return { total, delta };
+    const result = { id, total, delta };
+
+    this._onElapsed?.(result);
+    return result;
   }
 }
