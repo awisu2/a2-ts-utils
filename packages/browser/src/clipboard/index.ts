7cd38c3619eefa10ad195fd96d3214f218a8eab2
awisu2 <awisu2@gmail.com>
2026-05-24 11:37:09 +0900

    update clipboard functions

--- packages/browser/src/clipboard/index.ts
+++ packages/browser/src/clipboard/index.ts
@@ -1,3 +1,7 @@
-export function copyToClipboard(text: string) {
-  navigator.clipboard.writeText(text);
-}
+export const writTextToClipboardAsync = (text: string): Promise<void> => {
+  return navigator.clipboard.writeText(text);
+};
+
+export const readTextFromClipboardAsync = (): Promise<string> => {
+  return navigator.clipboard.readText();
+};
